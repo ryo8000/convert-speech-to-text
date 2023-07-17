01069ef4a409e07e3eaf54680ca26af46ff7a123
Ryo H <49798519+ryobs@users.noreply.github.com>
2023-07-17 13:33:14 +0900

    Update IAM role.

--- main.tf
+++ main.tf
@@ -14,23 +14,50 @@ data "archive_file" "zip" {
   source_dir  = "src"
 }
 
-data "aws_iam_policy_document" "policy" {
+data "aws_iam_policy_document" "assume_role_policy" {
   statement {
     sid    = ""
     effect = "Allow"
-
     principals {
       identifiers = ["lambda.amazonaws.com"]
       type        = "Service"
     }
-
     actions = ["sts:AssumeRole"]
   }
 }
 
+data "aws_partition" "current" {}
+
+data "aws_iam_policy" "aws_lambda_basic_execution_role" {
+  arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "aws_iam_policy" "amazon_transcribe_full_access" {
+  arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/AmazonTranscribeFullAccess"
+}
+
+data "aws_iam_policy" "amazon_s3_full_access" {
+  arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/AmazonS3FullAccess"
+}
+
 resource "aws_iam_role" "lambda_role" {
   name               = "TranscribeRole"
-  assume_role_policy = data.aws_iam_policy_document.policy.json
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "aws_lambda_basic_execution_role_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = data.aws_iam_policy.aws_lambda_basic_execution_role.arn
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_transcribe_full_access_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = data.aws_iam_policy.amazon_transcribe_full_access.arn
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_s3_full_access_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = data.aws_iam_policy.amazon_s3_full_access.arn
 }
 
 resource "aws_lambda_function" "lambda" {
